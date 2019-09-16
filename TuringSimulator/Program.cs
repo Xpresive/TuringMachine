4da285f2b1ebccddd1efc022316eecb6bfda049a
Xpresive <ltmainyk@gmail.com>
2019-09-16 22:56:53 +0300

    Add files via upload

---
+++ TuringSimulator/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace TuringSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            /*
+            int a, b;
+            char c;
+
+
+            string Input = Console.ReadLine();
+            char[] Tape = Input.ToCharArray();
+            // Console.WriteLine(Input);
+            int TapeLength = Input.Length;
+            int Head = 0;
+
+
+            while (true)
+            {
+                string Command = Console.ReadLine();
+                string[] split = Command.Split(' ');
+                a = Int32.Parse(split [0]);
+                b = Int32.Parse(split[1]);
+                c = Char.Parse(split[2]);
+                /*
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                Console.WriteLine(c);
+                
+                if (Tape[Head] == a)
+                {
+                    Tape[Head] = (char)b;
+                    if (c == 'R')
+                        Head++;
+                    else Head--;
+                }
+                else
+                    continue;
+
+
+            }
+        */
+        }
+    }
+    public class TuringMachine
+    {
+        public List<Instruction> Instructions = new List<Instruction>();
+
+        public TuringMachine()
+        {
+           
+        }
+
+
+        
+    }
+    
+    public class Instruction
+    {
+        string CurrentStatus;
+        string CurrentSymbol;
+        string NewSymbol;
+        string Direction;
+        string NewStatus;
+
+        public Instruction(string currentStatus, string currentSymbol, string newSymbol, string direction, string newStatus)
+        {
+            CurrentStatus = currentStatus;
+            CurrentSymbol = currentSymbol;
+            NewSymbol = newSymbol;
+            Direction = direction;
+            NewStatus = newStatus;
+        }
+
+        public Instruction() { }
+    }
+    
+}
